8a5361242c5bd2dc69957297ffd6783671129431
seunghyukcho <shhj1998@postech.ac.kr>
2021-03-21 20:39:53 +0900

    implemented weighted doctornet

--- model.py
+++ model.py
@@ -14,16 +14,52 @@ class FeatureExtractor(nn.Module):
         return feature
 
 
+class AveragingWeights(nn.Module):
+
+    def __init__(self, n_annotators, weight_type='W', feature_dim=2048, bottleneck_dim=None):
+        super(AveragingWeights, self).__init__()
+        self.weight_type = weight_type
+        if self.weight_type == 'W':
+            self.weights = nn.Parameter(torch.randn(n_annotators), requires_grad=True)
+        elif self.weight_type == 'I':
+            if bottleneck_dim is None:
+                self.weights = nn.Linear(feature_dim, n_annotators)
+            else:
+                self.weights = nn.Sequential(nn.Linear(feature_dim, bottleneck_dim), nn.Linear(bottleneck_dim, n_annotators))
+        else:
+            raise IndexError("weight type must be 'W' or 'I'.")
+
+    def __forward__(self, feature):
+        if self.weight_type == 'W':
+            return self.weights
+        else:
+            return self.weights(feature).view(-1)
+
+
 class DoctorNet(nn.Module):
 
-    def __init__(self, n_classes, n_annotators, feature_dim=2048):
+    def __init__(self, n_classes, n_annotators, weight_type='W', feature_dim=2048, bottleneck_dim=None):
         super(DoctorNet, self).__init__()
         self.feature_extractor = FeatureExtractor()
         self.annotators = nn.Parameter(torch.stack([torch.randn(feature_dim, n_classes) for _ in range(n_annotators)]), requires_grad=True)
+        self.weights = AveragingWeights(n_annotators, weight_type, feature_dim, bottleneck_dim)
         
-    def __forward__(self, x):
+    def __forward__(self, x, mask=None):
         feature = self.feature_extractor(x)
         decisions = torch.einsum('ik,jkl->ijl', feature, self.annotators)
 
-        return decisions
+        if mask is None:
+            return decisions
+        else:
+            weights = self.weights(feature)
+            predictions = decisions * weights[None, :, None]
+
+            predictions = predictions.masked_fill(~mask, 0)
+            predictions = torch.sum(predictions, axis=-1)
+
+            weights = weights.masked_fill(~mask, 0)
+            weights = torch.sum(weights, axis=-1)
+
+            predictions = predictions / weights[:, None]
+            return predictions
 
