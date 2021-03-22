64b8f2ea2abf8c78894638b1e5b570b8a18d1928
seunghyukcho <shhj1998@postech.ac.kr>
2021-03-22 16:46:30 +0900

    add transforms for input image

--- dataset.py
+++ dataset.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 from pathlib import Path
 from torch.utils.data import Dataset
-
+from torchvision import transforms
 
 class LabelMeDataset(Dataset):
     def __init__(self, root_dir, is_train=False):
@@ -10,6 +10,7 @@ class LabelMeDataset(Dataset):
         self.image_dir = self.root_dir / 'images'
         self.is_train = is_train
         self.labels = np.loadtxt(self.root_dir / 'labels.txt', dtype=np.float32)
+        self.transforms = transforms.Compose([transforms.Resize(299), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         
         with open(self.root_dir / 'filenames.txt', 'r') as f:
             self.image_paths = f.read().splitlines()
@@ -21,6 +22,7 @@ class LabelMeDataset(Dataset):
 
     def __getitem__(self, idx):
         x = Image.open(self.image_dir / self.image_paths[idx])
+        x = self.transforms(x)
         y = self.labels[idx]
 
         if self.is_train:
